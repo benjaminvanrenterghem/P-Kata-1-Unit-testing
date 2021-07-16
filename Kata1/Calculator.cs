18dc9aa94fde39f15827628909caa763753b1998
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2021-07-16 17:51:46 +0200

    Finished

---
+++ Kata1/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata1
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if(numbers.Length > 0)
+            {
+                Char separator = ',';
+                Char separatorII = '\n';
+
+                if (numbers.EndsWith(separator)) { numbers = numbers.Substring(0, numbers.Length - 1); } // verwijder trailing komma indien deze bestaat 
+                if (numbers.EndsWith(separatorII)) { numbers = numbers.Substring(0, numbers.Length - 2); }
+
+                string[] concatNumbers = numbers.Split(separator, separatorII);
+
+                int total = 0;
+                List<int> negatives = new List<int>();
+
+                foreach(string aspNumber in concatNumbers)
+                {
+                    int parsedNum = int.Parse(aspNumber);
+                    if (parsedNum < 0)
+                    {
+                        negatives.Add(parsedNum);
+                    } else if(parsedNum <= 1000) // verwaarlozing >1000
+                    {
+                        total += parsedNum;
+                    }
+                }
+
+                if(negatives.Count > 0)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("Negatives not allowed.\nRemove the following numbers: ");
+                    foreach(int i in negatives) { sb.Append($"{i}, "); }
+                    sb.Length = sb.Length - 2; // verwijder trailing komma en spatie 
+                    throw new NotSupportedException(sb.ToString());
+                }
+
+                return total;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Kata1Tests/CalculatorTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Kata1.Tests
+{
+    // AAA pattern
+    // Arrange → Act → Assert
+
+    [TestClass()]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        // TestInitialize zorgt er voor dat er voor iedere test met een schone lij begonnen wordt,
+        // was niet perse van toepassing sinds de staat van Calculator nooit veranderd (er is slechts sprake van 1 methode in een klasse die iets retourneert)
+        [TestInitialize]
+        public void Setup() 
+        {
+            _calculator = new Calculator();
+        }
+
+        private int AddTest(string toBeHandled) // wordt gebruikt door de onderstaande testmethodes
+        {
+            var res = _calculator.Add(toBeHandled);
+            return res;
+        }
+
+        [TestMethod()]
+        public void legeStringTest()
+        {
+            string testStr = "";
+            int perfres = 0;
+           
+            Assert.AreEqual(AddTest(testStr), perfres);
+        }
+
+        [TestMethod()]
+        public void enkelGetalTest()
+        {
+            string testStr = "8";
+            int perfres = 8;
+
+            Assert.AreEqual(AddTest(testStr), perfres);
+        }
+
+        [TestMethod()]
+        public void duoGetalMetSeparatorTest()
+        {
+            string testStr = "8,4";
+            int perfres = 12;
+
+            Assert.AreEqual(AddTest(testStr), perfres);
+        }
+
+        [TestMethod()]
+        public void nGetalMetSeparatorsTest()
+        {
+            string testStr = "4,3,6,9,4,1,3,2,1,5,1";
+            int perfres = 39;
+
+            Assert.AreEqual(AddTest(testStr), perfres);
+
+            // trailing komma ,
+            string testStrII = "4,3,6,9,4,1,3,2,1,5,1,";
+
+            Assert.AreEqual(AddTest(testStrII), perfres);
+        }
+
+        [TestMethod()]
+        public void nGetalMetDiffSeparatorsTest()
+        {
+            string testStr = "4\n3\n2\n1,3,4,5\n6";
+            int perfres = 28;
+
+            Assert.AreEqual(AddTest(testStr), perfres);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void nGetalMetNegatievenEnSeparatorsTest()
+        {
+            string testStr = "4\n-1,2,3,-7\n4";
+            int placeholder = 0;
+
+            Assert.AreEqual(AddTest(testStr), placeholder);
+
+        }
+
+        [TestMethod()]
+        public void nGetalMetDuizendenEnSeparatorsTest()
+        {
+            string testStr = "5,1000,1001,3";
+            int perfres = 1008;
+
+            Assert.AreEqual(AddTest(testStr), perfres);
+        }
+
+    }
+}
